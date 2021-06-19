b5e1614112000546810e9128d3bb8f214fbf3b0c
Nicole B <krizzabullecer@gmail.com>
2021-06-19 18:30:09 +0800

    build: set up path aliasing and sass (#3)
    * build: setup sass
    
    * fix: component inside head tag
    
    * chore: delete unused css
    
    * build: setup path aliasing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
-import Head from 'next/head'
-import '../styles/global.css';
+import Head from 'next/head';
+import '@/styles/global.scss';
 
 const App = ({ Component, pageProps }) => {
   return (
-    <Head>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <title>Pokemon Next!</title>
-      <Component {...pageProps} />;
-    </Head>
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Pokemon Next!</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
   );
 };
 
