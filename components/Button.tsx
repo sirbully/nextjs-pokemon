84145c6daa122e98302dd1649d2df3436c4edb33
Krizza Bullecer <krizzabullecer@gmail.com>
2021-06-19 23:58:30 +0800

    refactor: add button color prop

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,11 +2,12 @@ import { FC } from 'react';
 
 type ButtonProps = {
   type: 'submit' | 'button';
+  color: 'red' | 'blue';
 };
 
-const Button: FC<ButtonProps> = ({ children, type }) => (
+const Button: FC<ButtonProps> = ({ children, type, color }) => (
   <button
-    className="w-full border-2 border-black rounded-xl px-4 py-2 mt-4 focus:outline-none font-extrabold bg-red-500 hover:bg-red-600 text-white shadow-custom"
+    className={`w-full border-2 border-black rounded-xl px-4 py-2 mt-4 focus:outline-none font-extrabold bg-${color}-500 hover:bg-${color}-600 text-white shadow-custom`}
     type={type}
   >
     {children}
