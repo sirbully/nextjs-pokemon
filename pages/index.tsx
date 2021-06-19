d116e9b44918703c9b00abdbdcbf9fb8587e85a5
Krizza Bullecer <krizzabullecer@gmail.com>
2021-06-19 21:03:24 +0800

    feat: integrate auth context and react query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
-const Home = ({ allPostsData }) => {
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useAuth } from '@/hooks/auth-context';
+import styles from '@/styles/Redirect.module.scss';
+
+const Redirect = () => {
+  const router = useRouter();
+  const { state } = useAuth();
+
+  useEffect(() => {
+    if (state.isLoggedIn) router.push('/dashboard');
+    else router.push('/signin');
+  }, [state, router]);
+
   return (
-    <div>
-      hello
+    <div className="container mx-auto min-h-screen flex items-center justify-center">
+      <div className={styles.wrapper}>
+        <div className={styles.spinner} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home;
-
-export async function getStaticProps() {
-  const allPostsData = []
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
+export default Redirect;
