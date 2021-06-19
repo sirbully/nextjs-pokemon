3aa37fd691cecdb4f1656cd4b3946d337f9a13f5
Krizza Bullecer <krizzabullecer@gmail.com>
2021-06-20 01:21:51 +0800

    refactor: add button disabled prop

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,12 +3,14 @@ import { FC } from 'react';
 type ButtonProps = {
   type: 'submit' | 'button';
   color: 'red' | 'blue';
+  disabled: boolean;
 };
 
-const Button: FC<ButtonProps> = ({ children, type, color }) => (
+const Button: FC<ButtonProps> = ({ children, type, color, disabled }) => (
   <button
-    className={`w-full border-2 border-black rounded-xl px-4 py-2 mt-4 focus:outline-none font-extrabold bg-${color}-500 hover:bg-${color}-600 text-white shadow-custom`}
+    className={`w-full border-2 border-black rounded-xl px-4 py-2 mt-4 focus:outline-none font-extrabold bg-${color}-500 hover:bg-${color}-600 text-white shadow-custom disabled:opacity-50`}
     type={type}
+    disabled={disabled}
   >
     {children}
   </button>
