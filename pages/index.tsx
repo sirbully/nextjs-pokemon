f89485f8a37c2e2ba424dc97b91ba323e409189b
Krizza Bullecer <krizzabullecer@gmail.com>
2021-06-20 02:01:47 +0800

    chore: remove dead code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { useAuth } from '@/hooks/auth-context';
 import styles from '@/styles/Redirect.module.scss';
 
 const Redirect = () => {
   const router = useRouter();
-  const { state } = useAuth();
 
   useEffect(() => {
-    if (state.isLoggedIn) router.push('/dashboard');
-    else router.push('/signin');
-  }, [state, router]);
+    router.push('/signin');
+  }, [router]);
 
   return (
     <div className="container mx-auto min-h-screen flex items-center justify-center">
