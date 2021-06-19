a3cf55adbb2789753db5a7637953fd1977d4939b
Krizza Bullecer <krizzabullecer@gmail.com>
2021-06-20 02:00:21 +0800

    chore: revert

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,14 +3,12 @@ import { FC } from 'react';
 type ButtonProps = {
   type: 'submit' | 'button';
   color: 'red' | 'blue';
-  disabled: boolean;
 };
 
-const Button: FC<ButtonProps> = ({ children, type, color, disabled }) => (
+const Button: FC<ButtonProps> = ({ children, type, color }) => (
   <button
-    className={`w-full border-2 border-black rounded-xl px-4 py-2 mt-4 focus:outline-none font-extrabold bg-${color}-500 hover:bg-${color}-600 text-white shadow-custom disabled:opacity-50`}
+    className={`w-full border-2 border-black rounded-xl px-4 py-2 mt-4 focus:outline-none font-extrabold bg-${color}-500 hover:bg-${color}-600 text-white shadow-custom`}
     type={type}
-    disabled={disabled}
   >
     {children}
   </button>
