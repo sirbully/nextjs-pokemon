ce44f4124fdaceabe3c06af28d8e4696e1497f34
Krizza Bullecer <krizzabullecer@gmail.com>
2021-06-20 16:11:20 +0800

    fix: number too big on tablet

--- components/Card.tsx
+++ components/Card.tsx
@@ -17,10 +17,7 @@ type CardProps = {
 const Card: FC<CardProps> = ({ id, name, image, types }) => (
   <div className="rounded-2xl border-2 border-black shadow-custom p-8 mb-4 flex flex-col justify-center items-center hover:bg-gray-100 cursor-pointer">
     <div className="relative w-full flex justify-center">
-      <p
-        className="absolute -top-7 text-gray-300 font-extrabold"
-        style={{ fontSize: '100px' }}
-      >
+      <p className="absolute lg:top-0 md:top-3 text-gray-300 font-extrabold lg:text-8xl md:text-7xl text-8xl">
         #{padded(id)}
       </p>
       <Image src={image} alt={name} layout="fixed" width={96} height={96} />
