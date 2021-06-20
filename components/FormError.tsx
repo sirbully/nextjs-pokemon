5fb8be97df5ca6b0cb03e931d65c1efffe661d9a
Krizza Bullecer <krizzabullecer@gmail.com>
2021-06-20 20:07:26 +0800

    refactor: add alert role

--- components/FormError.tsx
+++ components/FormError.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
 
 const FormError: FC = ({ children }) => (
-  <span className="text-xs text-red-500 text-left mt-2">{children}</span>
+  <span role="alert" className="text-xs text-red-500 text-left mt-2">
+    {children}
+  </span>
 );
 
 export default FormError;
